b21ec9595a4890827a90e83fbd4494c326645574
Frietvorkje69 <1028402@hr.nl>
2024-05-10 19:58:53 +0200

    smooth scroll +  animate hero + text for project

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,18 +1,7 @@
-// smooth scroll
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
-
 // make header sticky
 window.addEventListener('scroll', function () {
-    var header = document.getElementById('header');
-    var navbar = document.getElementById('navbar');
+    let header = document.getElementById('header');
+    let navbar = document.getElementById('navbar');
     if (window.pageYOffset > header.offsetHeight) {
         navbar.classList.add('sticky');
     } else {
